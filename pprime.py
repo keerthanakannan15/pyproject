490a8e54d1d32bebd34a8e26081d16e809f179d5
keerthanakannan15 <keerthanaofskanna@gmail.com>
2019-04-02 15:42:59 +0530

    modified py

--- pprime.py
+++ pprime.py
@@ -1,11 +1,17 @@
-a=3
-if a > 1:
-    for x in range(2, a // 2):
-        print("x value is ", x)
-        if (a % x) == 0:
-            print(a, " not a prime")
-            break
-    else:
-        print(a, "prime no")
-else:
-    print("no")
+def prime(a):
+    for i in range(0, len(a)):
+        print("a[i] value is", a[i])
+        if a[i] > 1:
+            for x in range(2, a[i] // 2):
+                print("x value is ", x)
+                if (a[i] % x) == 0:
+                    print(a[i], " not a prime")
+                    break
+            else:
+                print(a[i], "prime no")
+        else:
+            print("no")
+
+
+n =[2 ,3 ,6 ,7]
+print(prime(n))
